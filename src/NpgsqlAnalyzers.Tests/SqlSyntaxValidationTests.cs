cbf570416509a2f3a7112dec4229e4720247c67f
Ivan Stoyanov <stoyanov.ivan98@gmail.com>
2020-04-11 10:46:23 +0200

    Add tests for sql syntax validation

---
+++ src/NpgsqlAnalyzers.Tests/SqlSyntaxValidationTests.cs
@@ -0,0 +1,135 @@
+using Microsoft.CodeAnalysis;
+using NpgsqlAnalyzers.Tests.Utils;
+using NUnit.Framework;
+using System;
+using ThrowawayDb.Postgres;
+
+namespace NpgsqlAnalyzers.Tests
+{
+    [TestFixture]
+    public class Tests : IDisposable
+    {
+        private readonly ThrowawayDatabase _database = Database.CreateDatabase();
+        private bool _isDisposed;
+
+        [Test]
+        public void AssertNonExistentQueryDetectedInConstructor()
+        {
+            string source = @"
+using Npgsql;
+
+namespace Testing
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            using var command = new NpgsqlCommand(""SELECT * FROM non_existent_table"");
+        }
+    }
+}
+";
+
+            Diagnostics.AnalyzeSourceCode(
+                source,
+                new NpgsqlAnalyzers(_database.ConnectionString),
+                new DiagnosticResult
+                {
+                    Id = "PSCA1000",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new DiagnosticResultLocation[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 10, 33),
+                    },
+                    Message = "The relation referenced at position '15' does not exist.",
+                });
+        }
+
+        [Test]
+        public void AssertNonExistentTableDetectedInDeclaration()
+        {
+            string source = @"
+using Npgsql;
+
+namespace Testing
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            string query = ""UPDATE bad_table SET id = 1 WHERE name = 'test';"";
+            using var command = new NpgsqlCommand(query);
+        }
+    }
+}
+";
+            Diagnostics.AnalyzeSourceCode(
+                source,
+                new NpgsqlAnalyzers(_database.ConnectionString),
+                new DiagnosticResult
+                {
+                    Id = "PSCA1000",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new DiagnosticResultLocation[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 10, 28),
+                    },
+                    Message = "The relation referenced at position '8' does not exist.",
+                });
+        }
+
+        [Test]
+        public void AssertNonExistentTableDetectedInReDeclaration()
+        {
+            string source = @"
+using Npgsql;
+
+namespace Testing
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            string query = ""SELECT * FROM users;"";
+            using var command = new NpgsqlCommand(query);
+
+            query = ""UPDATE bad_table SET id = 1 WHERE name = 'test';"";
+            using var command = new NpgsqlCommand(query);
+        }
+    }
+}
+";
+            Diagnostics.AnalyzeSourceCode(
+                source,
+                new NpgsqlAnalyzers(_database.ConnectionString),
+                new DiagnosticResult
+                {
+                    Id = "PSCA1000",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new DiagnosticResultLocation[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 13, 21),
+                    },
+                    Message = "The relation referenced at position '8' does not exist.",
+                });
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (!_isDisposed)
+            {
+                if (isDisposing)
+                {
+                    _database.Dispose();
+                }
+                _isDisposed = true;
+            }
+        }
+    }
+}
